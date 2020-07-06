d0b1fcf0e7065163425ed42684b3189c86c3c9c3
calvinjung <myadiuio@gmail.com>
2020-07-06 09:19:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client()
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
-    client.user.setActivity("YT | =help", {type: "WATCHING"});
+    client.user.setActivity("YouTube", {type: "WATCHING"});
 })
 
 var prefix = "="
