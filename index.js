98de851a7c39808430a80361bce645ea4c2b38f3
calvinjung <myadiuio@gmail.com>
2020-07-07 11:30:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ client.on("message", async message => {
         const diffvcskip = new Discord.MessageEmbed()
             .setColor(`#FFA500`)
             .setTitle(`You are not in the same VC with me!`)
-            .setDescription(`You have to be in the same vc with me to skip music.`)
+            .setDescription(`You have to be in the same VC with me to skip music.`)
 
         if (!message.member.voice.channel) return message.channel.send(novcskip);
         if(message.member.voice.channel && message.guild.me.voice.channel && message.member.voice.channel != message.guild.me.voice.channel) 
@@ -146,10 +146,12 @@ client.on("message", async message => {
             .setColor('#FFA500')
             .setTitle('Join a voice channel first!')
             .setDescription("You have to be in a voice channel to stop the music.")
+
         const diffvcstop = new Discord.MessageEmbed()
             .setColor(`#FFA500`)
             .setTitle(`You are not in the same VC with me!`)
-            .setDescription(`You have to be in the same vc with me to skip music.`)
+            .setDescription(`You have to be in the same VC with me to stop music.`)
+
         if (!message.member.voice.channel) return message.channel.send(novcstop);
         if(message.member.voice.channel && message.guild.me.voice.channel && message.member.voice.channel != message.guild.me.voice.channel) 
             return message.channel.send(diffvcstop)
