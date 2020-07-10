721c601aba289123992ea7df8a99aa9098b09e38
calvinjung <myadiuio@gmail.com>
2020-07-10 11:16:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -75,9 +75,10 @@ client.on("message", async message => {
         if(message.member.voice.channel && message.guild.me.voice.channel && message.member.voice.channel != message.guild.me.voice.channel) 
             return message.channel.send(diffvcr)
 
-        if(isNaN(amount) || amount+1 >= serverQueue.songs.length || !amount ||  amount <= 0 ) return message.channel.send(invr)
+        if(isNaN(amount) || amount >= serverQueue.songs.length || !amount ||  amount <= 0 ) return message.channel.send(invr)
 
         serverQueue.songs = serverQueue.songs.splice(amount+1, 1)
+        console.log(serverQueue.songs.join("\n"))
 
         const removed = new Discord.MessageEmbed()
             .setColor(`00ff00`)
