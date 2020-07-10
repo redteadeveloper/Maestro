35cf67c883b2e889cca329ec8653c9bb8c4e3e4f
calvinjung <myadiuio@gmail.com>
2020-07-10 09:43:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ client.on("message", async message => {
         if(songsarray.length > 0) {
             
             for (var j = 0; j < 1; j++) {
-                songsarray[j] = "⏯︎ ``" + songsarray[j] + "``\n"
+                songsarray[j] = ":play_pause: ``" + songsarray[j] + "``\n"
             }
             for (var i = 1; i < songsarray.length; i++) {
                 songsarray[i] = "**" + (i) + ".** ``"+ songsarray[i] + "``";
