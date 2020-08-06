e1fe28a8a02fe04526330f463953812ae496f6ff
calvinjung <myadiuio@gmail.com>
2020-08-07 00:38:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ client.on("message", async message => {
         const infol = new Discord.MessageEmbed() 
             .setColor(`#b19cd9`)
             .setTitle(`Lyrics command`)
-            .setDescription(`Semds the lyrics of a song.`)
+            .setDescription(`Sends the lyrics of a song.`)
             .setFooter(`Usage: &lyrics [search word]`)
 
         const G = new Genius.Client(process.env.GENIUSKEY)
