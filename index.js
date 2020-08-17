7daf287b4a50e6030d58262ecf5e4628505f2582
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-08-17 16:17:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js") 
 const ytdl = require("ytdl-core");
 const YouTube = require("discord-youtube-api");
 const Genius = require("genius-lyrics");
