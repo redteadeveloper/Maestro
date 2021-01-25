b82a2441051d395738ce0a0e48fcf50fda2d7470
RedTea <myadiuio@gmail.com>
2021-01-25 19:15:28 +0900

    Update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ytdl = require("ytdl-core");
 const YouTube = require("discord-youtube-api");
 const Genius = require("genius-lyrics");
 const mongoose = require('mongoose')
+const { inspect } = require('util');
 const Paginator = require('./modules/paginator.js')
 
 const client = new Discord.Client() 
@@ -495,6 +496,28 @@ client.on("message", async message => {
         
         message.channel.send(aliases)
 
+    } else if (command == 'eval') {
+        if (msg.author.id !== '611396886418685982') return;
+
+        const args1 = msg.content.split(' ').slice(1); 
+        const evalcmd = args1.join(' '); 
+
+        let evaled;
+
+        try {
+            evaled = await eval(`if (1>0) { ${evalcmd} }`);
+            msg.channel.send("```yaml\n" + 
+            inspect(evaled)
+            + "\n```").catch(error => {
+                msg.channel.send("Result too long, check logs.")
+            });
+            console.log("-- Inspection result --\n" + inspect(evaled) + "\n------------------------");
+        }
+        catch (error) {
+            console.error(error);
+            msg.reply('An error occurred during evaluation.');
+            msg.channel.send("```" + error + "```").catch(error => {msg.channel.send("Error too long, chack logs.")})
+        }
     }
 });
 
